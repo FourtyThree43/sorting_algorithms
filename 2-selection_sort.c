5b71fb2836d05e908c2bf95a3e7dfca49ddef0b7
FourtyThree43 <shaqmwa@outlook.com>
2023-04-26 01:19:08 +0300

    Task 2: select sort

---
+++ 2-selection_sort.c
@@ -0,0 +1,46 @@
+#include "sort.h"
+#include <stddef.h>
+
+/**
+ * swap - swaps two integers positions in as array.
+ * @x: First integer
+ * @y: Second integer
+ */
+void swap(int *x, int *y)
+{
+	int temp;
+
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ *                  using the Selection sort algorithm
+ * @array: list of elements to sort.
+ * @size: the size of the array to be sorted.
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min_index;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0 ; i < size - 1; i++)
+	{
+		min_index = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_index])
+				min_index = j;
+		}
+		if (min_index != 1)
+		{
+			swap(&array[i], &array[min_index]);
+			print_array(array, size);
+		}
+	}
+}
