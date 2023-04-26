239697daa2f961ef6a57b2211f2ff72fc93820ce
FourtyThree43 <shaqmwa@outlook.com>
2023-04-26 15:24:03 +0300

    Udated gap = 1

--- 100-main.c
+++ 100-main.c
@@ -9,12 +9,13 @@
  */
 int main(void)
 {
-	int array[] = {100, 19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	/*int array[] = {100, 19, 48, 99, 71, 13, 52, 96, 73, 86, 7};*/
+	int array[] = {2, 1};
 	size_t n = sizeof(array) / sizeof(array[0]);
 
 	print_array(array, n);
 	printf("\n");
-	shell_sort_Ciura(array, n);
+	shell_sort(array, n);
 	printf("\n");
 	print_array(array, n);
 	return (0);
