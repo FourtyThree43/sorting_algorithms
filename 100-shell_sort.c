de39824265ea132501ad150f1a1e81502245328d
FourtyThree43 <shaqmwa@outlook.com>
2023-04-29 19:45:23 +0300

    Update

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -30,7 +30,7 @@ void shell_sort(int *array, size_t size)
 	while (gap < (size / 3))
 		gap = gap * 3 + 1;
 
-	for (; gap > 0; gap /= 3)
+	for (; gap >= 1; gap /= 3)
 	{
 		for (i = gap; i < size; i++)
 		{
